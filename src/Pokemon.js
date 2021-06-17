ff05f864ddbaeb7e775e5384df003389d297e4e4
Matt Lane <lane.matthew@gmail.com>
2021-06-16 20:37:28 -0700

    add useeffect to pokemon

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,5 +1,24 @@
-function Pokemon() {
-  return <h1>Pokemon</h1>;
+import { useEffect, useState } from "react";
+
+function Pokemon({ name, url }) {
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    const catchOne = async () => {
+      let response = await fetch(url);
+      let data = await response.json();
+      setPokemon(data);
+    };
+
+    catchOne();
+  }, [url]);
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      {pokemon ? <img src={pokemon.sprites.front_default} alt={`${name} front default`} /> : null }
+    </div>
+  );
 }
 
 export default Pokemon;
